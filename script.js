ba3b977bf231d35645ad167b2773718d87b7add6
Michinosuke <michinosuke.info@gmail.com>
2022-11-09 02:17:24 +0900

    正答のみモードの追加

--- script.js
+++ script.js
@@ -1,65 +1,185 @@
-let isLoading = true
+// 問題の配列
+// answers: boolean[]
+// choiceLabelElements: Element[]
+// choiceLiElements: Element[]
+// element: Element
+// explanationElement: Element
+// selects: boolean[]
+// isExpand: boolean
+const questions = []
+let detailedResultPanel = null
+let buttonSeitoNomi
+let buttonIchimonItto
+let doneFirstReset = false
 
-const intervalId1 = setInterval(() => {
-    const wrapper = document.querySelector("div[data-purpose='detailed-result-panel']")
-    if (wrapper) {
-        isLoading = false
-        clearInterval(intervalId1)
-    }
-}, 100)
+const onClickChoice = (questionIndex, choiceIndex) => {
+    const question = questions[questionIndex]
+    question.selects[choiceIndex] = true
+    const correctCount = question.answers.filter(answer => answer).length
+    const selectedCount = question.selects.filter(select => select).length
+    const choiceLabelElements = question.choiceLabelElements
+    const answers = question.answers
+    const explanationElement = question.explanationElement
 
-const intervalId2 = setInterval(() => {
-    if (isLoading) return
-    clearInterval(intervalId2)
+    console.log({correctCount})
+    console.log({selectedCount})
 
-    const wrapper = document.querySelector("div[data-purpose='detailed-result-panel']")
-
-    const qs = []
-    // color
-    const questions = [...wrapper.querySelectorAll(':scope > div')].filter((div) => div.className.includes('question'))
-    questions.forEach((question, questionIndex) => {
-        const choices = [...question.querySelectorAll('label')]
-        const explanation = [...question.querySelectorAll('div')].find((div) => div.className.includes('explanation'))
-        explanation.classList.add('display-none')
-        const q = {
-            element: question,
-            choiceElements: choices,
-            answers: [],
-            selects: choices.map(() => null),
-            explanationElement: explanation
-        }
-        choices.forEach((choice, choiceIndex) => {
-            let isCorrect = false
-            const classList = [...choice.classList]
-            classList.forEach((klass) => {
-                if (klass.match(/(?<!in)correct/)) {
-                    isCorrect = true
-                }
-                if (klass.match(/correct/)) {
-                    choice.classList.replace(klass, klass + '----')
-                }
-            })
-            choice.addEventListener('click', (e) => {
-                qs[questionIndex].selects = qs[questionIndex].selects.map(() => false)
-                qs[questionIndex].selects[choiceIndex] = true
-                changeQuestionUI(qs[questionIndex])
-            })
-            q.answers.push(isCorrect)
+    if (!question.isExpand) {
+        choiceLabelElements.forEach((choiceElement, choiceIndex) => {
+            if (question.selects[choiceIndex]) {
+                choiceElement.classList.add('ui-selected')
+            }
         })
-        qs.push(q)
+    }
+
+    if (!question.isExpand && selectedCount < correctCount) {
+        return
+    }
+
+    question.isExpand = !question.isExpand
+
+    if (question.isExpand) {
+        question.selects = question.selects.map(() => false)
+        explanationElement.classList.remove('display-none')
+        explanationElement.classList.add('display-active')
+        choiceLabelElements.forEach((choiceElement, choiceIndex) => {
+            choiceElement.classList.remove('background-none')
+            if (answers[choiceIndex]) {
+                choiceElement.classList.add('ui-correct')
+            } else {
+                choiceElement.classList.add('ui-incorrect')
+            }
+        })
+    } else {
+        question.selects = question.selects.map(() => null)
+        explanationElement.classList.add('display-none')
+        explanationElement.classList.remove('display-active')
+        choiceLabelElements.forEach((choiceElement, choiceIndex) => {
+            choiceElement.classList.remove('ui-incorrect')
+            choiceElement.classList.remove('ui-correct')
+            choiceElement.classList.add('background-none')
+            choiceElement.classList.remove('ui-selected')
+        })
+    }
+}
+
+const setMode = (mode) => {
+    reset()
+
+    buttonSeitoNomi.classList.remove('selected')
+    buttonIchimonItto.classList.remove('selected')
+
+    switch(mode) {
+        case 'ICHIMON_ITTO':
+            buttonIchimonItto.classList.add('selected')
+            onModeIchimonItto()
+            break
+        case 'SEITO_NOMI':
+            buttonSeitoNomi.classList.add('selected')
+            onModeSeitoNomi()
+            break
+    }
+}
+
+const onModeSeitoNomi = () => {
+    questions.forEach(question => {
+        question.explanationElement.classList.add('display-none')
+        question.choiceLiElements.forEach((choiceElement, choiceIndex) => {
+            if (!question.answers[choiceIndex]) {
+                choiceElement.classList.add('display-none')
+            }
+        })
+        question.choiceLabelElements.forEach((choiceLabelElement, choiceIndex) => {
+            if (question.answers[choiceIndex]) {
+                choiceLabelElement.addEventListener('click', question.choiceCallbacks[choiceIndex])
+            }
+        })
+    })
+}
+
+const onModeIchimonItto = () => {
+    questions.forEach(question => {
+        question.explanationElement.classList.add('display-none')
+        question.choiceLabelElements.forEach((choiceElement, choiceIndex) => {
+            choiceElement.addEventListener('click', question.choiceCallbacks[choiceIndex])
+        })
+    })
+}
+
+const reset = () => {
+    if (!doneFirstReset) {
+        const questionElements = [...detailedResultPanel.querySelectorAll(':scope > div')].filter((div) => div.className.includes('question'))
+        questionElements.forEach((question, questionIndex) => {
+            const choiceLabelElements = [...question.querySelectorAll('label')]
+            const choiceLiElements = [...question.querySelectorAll('li')]
+            const explanation = [...question.querySelectorAll('div')].find((div) => div.className.includes('explanation'))
+            const q = {
+                element: question,
+                choiceLabelElements,
+                choiceLiElements,
+                choiceCallbacks: choiceLabelElements.map((_, i) => () => onClickChoice(questionIndex, i)),
+                answers: [],
+                selects: choiceLabelElements.map(() => null),
+                explanationElement: explanation,
+                isExpand: false
+            }
+            choiceLabelElements.forEach((choice, choiceIndex) => {
+                let isCorrect = false
+                const classList = [...choice.classList]
+                classList.forEach((klass) => {
+                    if (klass.match(/(?<!in)correct/)) {
+                        isCorrect = true
+                    }
+                    if (klass.match(/correct/)) {
+                        choice.classList.replace(klass, klass + '----')
+                    }
+                })
+                q.answers.push(isCorrect)
+            })
+            questions.push(q)
+        })
+
+        const statusButtons = [...detailedResultPanel.querySelectorAll(':scope div')].filter((div) => div.innerHTML === '未回答')
+        statusButtons.forEach(statusButton => statusButton.classList.add('display-none'))
+
+        const title = [...detailedResultPanel.querySelectorAll(':scope div')].find((div) => div.innerHTML.match(/^\d+回目の試み$/))
+        title.innerHTML = 'Udemy Interactive'
+    }
+
+    detailedResultPanel.classList.add('detailed-result-panel')
+
+    questions.forEach(question => {
+        question.explanationElement.classList.remove('display-none')
+        question.explanationElement.classList.remove('display-active')
+        question.choiceLabelElements.forEach((choiceLabelElement, choiceIndex) => {
+            choiceLabelElement.classList.remove('ui-correct')
+            choiceLabelElement.classList.remove('ui-incorrect')
+            choiceLabelElement.classList.remove('ui-selected')
+            choiceLabelElement.removeEventListener('click', question.choiceCallbacks[choiceIndex])
+        })
+        question.choiceLiElements.forEach((choiceLiElement) => {
+            choiceLiElement.classList.remove('display-none')
+        })
+        question.selects = question.selects.map(() => null)
+        question.isExpand = false
     })
 
-    const changeQuestionUI = (question) => {
-        const choiceElements = question.choiceElements
-        const answers = question.answers
-        const selects = question.selects
-        const explanationElement = question.explanationElement
-        choiceElements.forEach((choiceElement, choiceIndex) => {
-            const uiClass = answers[choiceIndex] ? 'ui-correct' : 'ui-incorrect'
-            choiceElement.classList.toggle(uiClass)
-        })
-        explanationElement.classList.toggle('display-none')
-        explanationElement.classList.toggle('display-active')
+    const footer = document.querySelector('footer')
+
+    if (!buttonSeitoNomi) {
+        buttonSeitoNomi = document.createElement('button')
+        buttonSeitoNomi.innerHTML = '正答のみ'
+        buttonSeitoNomi.classList.add('mode-button', 'ud-btn', 'udlite-btn', 'ud-btn-large', 'udlite-btn-large', 'ud-btn-secondary', 'udlite-btn-secondary')
+        buttonSeitoNomi.addEventListener('click', () => setMode('SEITO_NOMI'))
+        footer.prepend(buttonSeitoNomi)
+    }
+
+    if (!buttonIchimonItto) {
+        buttonIchimonItto = document.createElement('button')
+        buttonIchimonItto.innerHTML = '一問一答'
+        buttonIchimonItto.classList.add('mode-button', 'ud-btn', 'udlite-btn', 'ud-btn-large', 'udlite-btn-large', 'ud-btn-secondary', 'udlite-btn-secondary')
+        buttonIchimonItto.addEventListener('click', () => setMode('ICHIMON_ITTO'))
+        footer.prepend(buttonIchimonItto)
     }
 
     // hide check
@@ -69,4 +189,18 @@ const intervalId2 = setInterval(() => {
     // hide text
     const texts = [...document.querySelectorAll('div.udlite-heading-sm')]
     texts.forEach(text => text.classList.add('display-none'))
+
+    doneFirstReset = true
+}
+
+const onLoadPage = () => {
+    setMode('ICHIMON_ITTO')
+}
+
+const waitAppearQuestionsId = setInterval(() => {
+    detailedResultPanel = document.querySelector("div[data-purpose='detailed-result-panel']")
+    if (detailedResultPanel) {
+        onLoadPage()
+        clearInterval(waitAppearQuestionsId)
+    }
 }, 100)
